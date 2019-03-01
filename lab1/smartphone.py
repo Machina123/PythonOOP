5cff41e49c89bc5b078ae074a72a8745517509fc
Patryk Ciepiela <machina123@boun.cr>
2019-03-01 18:27:43 +0100

    Lab1 task 4 upload

---
+++ lab1/smartphone.py
@@ -0,0 +1,51 @@
+import pickle
+
+class Smartphone:
+
+    def __init__(self, manufacturer, model, price):
+        self._manufacturer = manufacturer
+        self._model = model
+        self._price = price
+
+    def get_manufacturer(self):
+        return self._manufacturer
+
+    def set_manufacturer(self, newmfg):
+        self._manufacturer = newmfg
+
+    def get_model(self):
+        return self._model
+
+    def set_model(self, newmdl):
+        self._model = newmdl
+
+    def get_price(self):
+        return self._price
+
+    def set_price(self, newprc):
+        self._price = newprc
+
+if __name__ == '__main__':
+    list_sp = []
+    galaxy = Smartphone("Samsung", "Galaxy S10", 3999)
+    apple = Smartphone("Apple", "iPhone XS", 4299)
+    the_better_one = Smartphone("Xiaomi", "Mi 8", 2299)
+    list_sp.append(galaxy)
+    list_sp.append(apple)
+    list_sp.append(the_better_one)
+
+    print("Lista przed peklowaniem:")
+    for i in range(len(list_sp)):
+        print(f"Smartfon #{i+1}: {list_sp[i].get_manufacturer()} {list_sp[i].get_model()}, cena: {list_sp[i].get_price()}")
+
+    phone_file = open("phones.dat", "wb")
+    pickle.dump(list_sp, phone_file, protocol=pickle.HIGHEST_PROTOCOL)
+    phone_file.close()
+
+    phone_file = open("phones.dat", "rb")
+    list_sp_unp = pickle.load(phone_file)
+    phone_file.close()
+
+    print("Lista po odpeklowaniu:")
+    for i in range(len(list_sp_unp)):
+        print(f"Smartfon #{i+1}: {list_sp_unp[i].get_manufacturer()} {list_sp_unp[i].get_model()}, cena: {list_sp_unp[i].get_price()}")
